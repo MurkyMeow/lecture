8de6120861dcc0c2e2cebd41c1ff04619ca2fa6c
pCoLDq <elvisisaev@gmail.com>
2019-07-29 16:38:53 +0300

    Add base models

--- MainApp/admin.py
+++ MainApp/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ("title")
+    list_display_links = ("title")
+    search_fields = ("title")
+
+class ExerciseAdmin(admin.ModelAdmin):
+    list_display = ("for_article")
+    list_display_links = ("for_article")
+    search_fields = ("for_article", "task")
+
+class AnswerAdmin(admin.ModelAdmin):
+    list_display = ("for_exercise")
+    list_display_links = ("for_exercise")
+    search_fields = ("for_exercise", "option", "correct")
+
+admin.site.register(Article, ArticleAdmin)
+admin.site.register(Exercise, ExerciseAdmin)
+admin.site.register(Answer, AnswerAdmin)
--- MainApp/models.py
+++ MainApp/models.py
@@ -1,3 +1,41 @@
 from django.db import models
 
 # Create your models here.
+class Article(models.Model):
+
+    title = models.CharField(max_length=300)
+    explanation = models.TextField()
+
+    class Meta:
+        verbose_name = "Article"
+        verbose_name_plural = "Articles"
+
+    def __str__(self):
+        return self.title
+
+
+class Exercise(models.Model):
+
+    for_article = models.ForeignKey("Article", on_delete=models.CASCADE)
+    task = models.TextField()
+
+    class Meta:
+        verbose_name = "Exercise"
+        verbose_name_plural = "Exercises"
+
+    def __str__(self):
+        return self.name
+
+
+class Answer(models.Model):
+
+    for_exercise = models.ForeignKey("Exercise", on_delete=models.CASCADE)
+    option = models.CharField(max_length=100)
+    correct = models.BooleanField()
+
+    class Meta:
+        verbose_name = "Answer"
+        verbose_name_plural = "Answers"
+
+    def __str__(self):
+        return self.name
