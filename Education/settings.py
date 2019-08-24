eec5454e41364af2683ad319b2570b73ce993d3c
pCoLDq <elvisisaev@gmail.com>
2019-08-24 13:02:57 +0300

    Add secretkey file.

--- Education/settings.py
+++ Education/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 's-%#kbraw7yj&rusmxc7-afpvm=14dqvo9uxfz9i746d!!%f5u'
+with open('secret_key.txt') as f:
+    SECRET_KEY = f.read().strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
