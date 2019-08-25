3a7286e52b098b1108852a3da020013d760603e7
pCoLDq <elvisisaev@gmail.com>
2019-08-25 13:03:37 +0300

    Add tests, validation.

--- MainApp/tests.py
+++ MainApp/tests.py
@@ -11,7 +11,7 @@ user = {
 
 class CommentsTests(APITestCase):
     def setUp(self):
-        u = User.objects.create(
+        u = User.objects.create_user(
             username=user['name'], email=user['email'], password=user['password']
         )
         self.comments = [
@@ -27,7 +27,7 @@ class CommentsTests(APITestCase):
         self.assertEqual(len(res.data), len(self.comments))
 
     def test_create_comment(self):
-        comment = { 'lecture_id': 2, 'slide_id': 1, 'text': 'qwerqwer' }
+        comment = { 'user': User.objects.get(email=user['email']), 'lecture_id': 2, 'slide_id': 1, 'text': 'qwerqwer' }
         res = self.client.post('/course/comments/', comment)
         self.assertEqual(res.status_code, status.HTTP_200_OK)
         self.assertEqual(res.data['text'], comment['text'])
@@ -44,3 +44,24 @@ class CommentsTests(APITestCase):
         comment = { 'comment_id': 1 }
         res = self.client.delete('/course/comments/', comment)
         self.assertEqual(res.status_code, status.HTTP_204_NO_CONTENT)
+
+class CommentsValidatonTests(APITestCase):
+    def setUp(self):
+        u = User.objects.create_user(
+            username=user['name'], email=user['email'], password=user['password']
+        )
+        self.comments = [
+            Comment.objects.create(user=u, lecture_id=1, slide_id=1, text='foo'),
+            Comment.objects.create(user=u, lecture_id=1, slide_id=1, text='bar'),
+        ]
+        self.client.force_authenticate(u)
+
+    def test_create_comment(self):
+        comment = { 'user': User.objects.get(email=user['email']) ,'lecture_id': 1, 'slide_id': 1, 'text': '' }
+        res = self.client.post('/course/comments/', comment)
+        self.assertEqual(res.status_code, status.HTTP_406_NOT_ACCEPTABLE)
+
+    def test_delete_comment(self):
+        comment = { 'comment_id': 101 }
+        res = self.client.delete('/course/comments/', comment)
+        self.assertEqual(res.status_code, status.HTTP_404_NOT_FOUND)
--- MainApp/views.py
+++ MainApp/views.py
@@ -6,14 +6,22 @@ from rest_framework.response import Response
 from .models import *
 from .serializers import *
 
+
 def LectureView(request, course, lecture):
     return render(request, 'lecture-temlpate.html', context={
       'course': course,
       'lecture': lecture,
     })
 
+def EmptyStringValidator(string):
+    if string == '' or string == None:
+        return Response(status=status.HTTP_406_NOT_ACCEPTABLE)
+
 class APIComments(APIView):
     def post(self, request):
+        #if request.data.get('text') == '' or request.data.get('text') == None:
+        #    return Response(status=status.HTTP_406_NOT_ACCEPTABLE)
+        return EmptyStringValidator(request.data.get('text'))
         new_comment = Comment.objects.create(
             user=request.user,
             lecture_id=request.data.get('lecture_id'),
@@ -32,16 +40,26 @@ class APIComments(APIView):
         return Response(serializer.data)
 
     def patch(self, request):
-        comment = Comment.objects.get(pk=request.data.get('comment_id'))
+        try:
+            comment = Comment.objects.get(pk=request.data.get('comment_id'))
+        except Comment.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        return EmptyStringValidator(request.data.get('text'))
+        #if request.data.get('text') == '' or request.data.get('text') == None:
+        #    return Response(status=status.HTTP_406_NOT_ACCEPTABLE)
         comment.text = request.data.get('text')
         comment.save()
         serializer = CommentSerializer(comment)
         return Response(serializer.data)
 
     def delete(self, request):
-        comment = Comment.objects.get(pk=request.data.get('comment_id'))
+        try:
+            comment = Comment.objects.get(pk=request.data.get('comment_id'))
+        except Comment.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
         comment.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+
 
 class APIProgress(APIView):
     def post(self, request):
