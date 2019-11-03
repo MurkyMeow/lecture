361057c8d9bba49084fe8573e9a38c9b34f37aee
MurkyMeow <dinosowermurky@gmail.com>
2019-11-04 01:37:46 +0900

    remove no longer needed files in AuthApp

--- AuthApp/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path, include
-from .views import *
-
-urlpatterns = [
-    path('signin/', SigninView),
-    path('signup/', SignupView),
-    path('userdata/', UserDataView),
-    path('logout/', LogoutView),
-]
