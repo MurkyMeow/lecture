1810fdd9d5e0f9dc3d26689641e8ee713c5a1634
MurkyMeow <dinosowermurky@gmail.com>
2019-11-04 00:47:44 +0900

    setup graphene_django, add some views

--- Education/urls.py
+++ Education/urls.py
@@ -1,18 +1,28 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic.base import TemplateView
-from rest_framework import permissions
-from drf_yasg.views import get_schema_view
-from drf_yasg import openapi
+from graphene_django.views import GraphQLView
+import graphene
+import MainApp.schema
 
-docs_view = get_schema_view(
-    openapi.Info(title="Api docs", default_version='v1'),
-)
+# This class will inherit from multiple Queries
+# as we begin to add more apps to our project
+class Query(
+    MainApp.schema.Query,
+    graphene.ObjectType
+):
+    pass
+
+class Mutation(
+    MainApp.schema.Mutation,
+    graphene.ObjectType
+):
+    pass
+
+schema = graphene.Schema(query=Query, mutation=Mutation)
 
 urlpatterns = [
-    path('api-docs', docs_view.with_ui('swagger')),
-    path('course/', include('MainApp.urls')),
-    path('auth/', include('AuthApp.urls')),
     path('admin/', admin.site.urls),
+    path('graphql/', GraphQLView.as_view(graphiql=True, schema=schema)),
     path('', TemplateView.as_view(template_name='index.html')),
 ]
