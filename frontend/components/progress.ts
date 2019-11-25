bb82f73ee1e6e838e7bfcdf0c13d612747fd3a09
MurkyMeow <dinosowermurky@gmail.com>
2019-11-25 20:00:42 +0900

    redesign progress component

--- frontend/components/progress.ts
+++ frontend/components/progress.ts
@@ -1,24 +1,18 @@
-import { customElement, PipeFn, defAttr, styleEl, repeat, on, attr, TypedDispatch } from 'marycat'
-import { span } from '../bindings'
+import { customElement, PipeFn, defAttr, styleEl, style } from 'marycat'
+import { div } from '../bindings'
 import css from './progress.css'
-
-type ProgressDispatch =
-  TypedDispatch<{ change: number }>
 
 function viewProgress(h: PipeFn<ShadowRoot>, {
   max = defAttr(4),
-  active = defAttr(-1),
-  done = defAttr<number[]>([]),
-}, t_dispatch: ProgressDispatch) {
-  const items = max.map(v => [...Array(v).keys()])
-    return h
-    (styleEl()(css))
-    (repeat(items, x => x.toString(), (_, i) =>
-      (span()
-        (on('click', () => h(t_dispatch('change', i.v))))
-        (attr('data-done', done.map(v => v.includes(i.v))))
-        (attr('data-active', active.eq(i)))
-      )
-    ))
+  done = defAttr(0),
+}) {
+  return h
+  (styleEl()(css))
+  (div('.bar-wrapper')
+    (div('.bar')
+      (style('--max', max.map(v => `${v}px`)))
+      (style('--done', done.map(v => `${v}px`)))
+    )
+  )
 }
 export const Progress = customElement('lecture-progress', viewProgress)
