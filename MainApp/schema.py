e0859ce219683a68d1618c99ddb6fa41ad99883e
pCoLDq <elvisisaev@gmail.com>
2019-12-08 13:05:34 +0300

    GQL: add 2 more types(Lecture, Slide), 1 mutation(CreateProgress), 2 resolvers(lectures, slides)

--- MainApp/schema.py
+++ MainApp/schema.py
@@ -1,6 +1,9 @@
 import graphene
 from graphene_django import DjangoObjectType
-from .models import Comment, Course, Progress
+from .models import *
+
+def validate_comment(data):
+    if not data.get('text'): raise ValidationError('`text` cant be empty')
 
 class CourseType(DjangoObjectType):
   class Meta:
@@ -9,6 +12,18 @@ class CourseType(DjangoObjectType):
 class CommentType(DjangoObjectType):
   class Meta:
     model = Comment
+
+class ProgressType(DjangoObjectType):
+  class Meta:
+    model = Progress
+
+class LectureType(DjangoObjectType):
+  class Meta:
+    model = Lecture
+
+class SlideType(DjangoObjectType):
+  class Meta:
+    model = Slide
 
 class CreateComment(graphene.Mutation):
   class Arguments:
@@ -19,16 +34,27 @@ class CreateComment(graphene.Mutation):
   comment = graphene.Field(CommentType)
 
   def mutate(self, info, text, slide_id, lecture_id):
+    validate_comment(request.data)
     return CreateComment(comment=Comment.objects.create(
       user=info.context.user,
       text=text,
-      slide_id=slide_id,
-      lecture_id=lecture_id,
+      slide=slide_id,
+      lecture=lecture_id,
     ))
 
-class ProgressType(DjangoObjectType):
-  class Meta:
-    model = Progress
+class CreateProgress(graphene.Mutation):
+  class Arguments:
+    slide_id = graphene.Int(required=True)
+    lecture_id = graphene.Int(required=True)
+
+  progress = graphene.Field(CommentType)
+
+  def mutate(self, info, text, slide_id, lecture_id):
+    return CreateProgress(progress=Progress.objects.create(
+      user=info.context.user,
+      slide=slide_id,
+      lecture=lecture_id,
+    ))
 
 class Query(graphene.ObjectType):
   comments = graphene.List(
@@ -38,9 +64,15 @@ class Query(graphene.ObjectType):
   )
   courses = graphene.List(CourseType)
   progress = graphene.List(ProgressType)
+  lectures = graphene.List(LectureType, 
+    course_id=graphene.Int(required=True)
+  )
+  slides = graphene.List(SlideType, 
+    course_id=graphene.Int(required=True)
+  )
 
   def resolve_comments(self, info, lecture_id, slide_id):
-    return Comment.objects.filter(lecture_id=lecture_id, slide_id=slide_id)
+    return Comment.objects.filter(lecture=lecture_id, slide=slide_id)
 
   def resolve_courses(self, info):
     return Course.objects.all()
@@ -48,5 +80,12 @@ class Query(graphene.ObjectType):
   def resolve_progress(self, info):
     return Progress.objects.filter(user=info.context.user)
 
+  def resolve_lectures(self, info, course_id):
+    return Lecture.objects.filter(course=course_id)
+
+  def resolve_slides(self, info, course_id):
+    return Slide.objects.filter(course=course_id)
+
 class Mutation(graphene.ObjectType):
   create_comment = CreateComment.Field()
+  create_progress = CreateProgress.Field()
